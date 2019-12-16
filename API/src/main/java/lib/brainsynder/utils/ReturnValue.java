8caba3ec82a4e9a627cd1f38f9277fbd379c3456
brainsynder <briansnyder498@gmail.com>
2019-12-16 08:48:12 -0800

    Added some utility classes

---
+++ API/src/main/java/lib/brainsynder/utils/ReturnValue.java
@@ -0,0 +1,5 @@
+package lib.brainsynder.utils;
+
+public interface ReturnValue<T> {
+    void run(T value);
+}
---
+++ API/src/main/java/lib/brainsynder/utils/WebConnector.java
@@ -0,0 +1,67 @@
+package lib.brainsynder.utils;
+
+import org.bukkit.plugin.Plugin;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.concurrent.CompletableFuture;
+
+public class WebConnector {
+    public static void getOutputStream(String link, Plugin plugin, ReturnValue<OutputStream> streamReturn) {
+        CompletableFuture.runAsync(() -> {
+            try {
+                System.setProperty("http.agent", "Chrome");
+                URL url = new URL(link);
+                URLConnection connection = url.openConnection();
+                connection.addRequestProperty("User-Agent", "Mozilla/5.0");
+                connection.addRequestProperty("Content-Encoding", "gzip");
+                connection.setConnectTimeout(10000);
+                connection.setReadTimeout(10000);
+                connection.setDoInput(true);
+                connection.setDoOutput(true);
+                new BukkitRunnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            streamReturn.run(connection.getOutputStream());
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }.runTask(plugin);
+            } catch (Exception ignored) {
+            }
+        });
+    }
+
+    public static void getInputStream(String link, Plugin plugin, ReturnValue<InputStream> streamReturn) {
+        CompletableFuture.runAsync(() -> {
+            try {
+                System.setProperty("http.agent", "Chrome");
+                URL url = new URL(link);
+                URLConnection connection = url.openConnection();
+                connection.addRequestProperty("User-Agent", "Mozilla/5.0");
+                connection.addRequestProperty("Content-Encoding", "gzip");
+                connection.setConnectTimeout(10000);
+                connection.setReadTimeout(10000);
+                connection.setDoInput(true);
+                connection.setDoOutput(true);
+                new BukkitRunnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            streamReturn.run(connection.getInputStream());
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }.runTask(plugin);
+            } catch (Exception ignored) {
+            }
+        });
+    }
+}
