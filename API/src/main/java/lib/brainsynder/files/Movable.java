0cf2917aec38ad74417e8af2a2884bec1b8010e3
brainsynder <briansnyder498@gmail.com>
2020-08-02 09:36:31 -0400

    Just added an other move method for Movable.java

--- API/src/main/java/lib/brainsynder/files/Movable.java
+++ API/src/main/java/lib/brainsynder/files/Movable.java
@@ -1,5 +1,11 @@
 package lib.brainsynder.files;
 
+import java.util.function.BiConsumer;
+
 public interface Movable {
     void move (String oldKey, String newKey);
+    default void move (String oldKey, String newKey, BiConsumer<String, String> onMove){
+        move(oldKey, newKey);
+        onMove.accept(oldKey, newKey);
+    }
 }
