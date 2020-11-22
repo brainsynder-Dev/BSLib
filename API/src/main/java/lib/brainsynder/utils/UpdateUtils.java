6f6b98158099e6012102873e576ffaefb210c12e
Brian <briansnyder498@gmail.com>
2020-11-22 00:09:42 -0500

    Changes to UpdateUtils

--- API/src/main/java/lib/brainsynder/utils/UpdateUtils.java
+++ API/src/main/java/lib/brainsynder/utils/UpdateUtils.java
@@ -78,35 +78,5 @@ public class UpdateUtils {
         });
     }
 
-    public class UpdateResult {
-        private final ReturnValue<JsonObject> newBuild;
-        private final Runnable onError;
-        private final Runnable noNewBuilds;
 
-        public UpdateResult (ReturnValue<JsonObject> newBuild) {
-            this (newBuild, () -> {});
-        }
-
-        public UpdateResult (ReturnValue<JsonObject> newBuild, Runnable noNewBuilds) {
-            this (newBuild, () -> {}, () -> {});
-        }
-
-        public UpdateResult (ReturnValue<JsonObject> newBuild, Runnable noNewBuilds, Runnable onError) {
-            this.newBuild = newBuild;
-            this.noNewBuilds = noNewBuilds;
-            this.onError = onError;
-        }
-
-        public ReturnValue<JsonObject> getNewBuild() {
-            return newBuild;
-        }
-
-        public Runnable getNoNewBuilds() {
-            return noNewBuilds;
-        }
-
-        public Runnable getOnError() {
-            return onError;
-        }
-    }
 }
