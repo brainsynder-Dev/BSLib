f07afda4b3f9184b0d3db3e6ca5bd3c21c71aa19
Brian <briansnyder498@gmail.com>
2021-01-30 11:38:52 -0500

    Added Colorize.containsHexColors(String) and Colorize.removeHexColor(String)

--- API/src/main/java/lib/brainsynder/utils/Colorize.java
+++ API/src/main/java/lib/brainsynder/utils/Colorize.java
@@ -8,7 +8,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Colorize {
-    private static final Pattern HEX_PATTERN = Pattern.compile("&#(\\w{5}[0-9a-f])");
+    private static final Pattern HEX_PATTERN = Pattern.compile("/&#(\\w{5}[0-9a-f])/gmi");
     private static Method of;
 
     static {
@@ -46,6 +46,20 @@ public class Colorize {
     }
 
     /**
+     * Will check if the {@param text} contains valid HEX colors '&#FFFFFF'
+     *
+     * @param text - text being checked
+     * @return
+     *      - true - contains hex
+     *      - false - no valid hex was found
+     */
+    public static boolean containsHexColors (String text) {
+        if ((text == null) || text.isEmpty()) return false;
+        Matcher matcher = HEX_PATTERN.matcher(text);
+        return matcher.find();
+    }
+
+    /**
      * Translates the {@param text} that use the '&' symbol
      * It also allows for hex colors (Example: '&#FFFFFF' = white)
      *
@@ -71,5 +85,42 @@ public class Colorize {
         }
 
         return ChatColor.translateAlternateColorCodes('&', matcher.appendTail(buffer).toString());
+    }
+
+    /**
+     * Removes the formatting if the Hex color '&x&e&3&a&a&4&f'
+     * And makes it more readable '&#e3aa4f'
+     *
+     * @param text - text to have hex removed from
+     * @return - finalized string
+     */
+    public static String removeHexColor (String text) {
+        // String is empty
+        if ((text == null) || text.isEmpty()) return text;
+
+        // The String does not contain any valid hex
+        if (!containsHexColors(text)) return text;
+
+        // Replaces the COLOR_CHAR('§') to '&'
+        text = text.replace(ChatColor.COLOR_CHAR, '&');
+        Pattern word = Pattern.compile("&x");
+        Matcher matcher = word.matcher(text);
+
+        char[] chars = text.toCharArray();
+        while(matcher.find()) {
+            StringBuilder builder = new StringBuilder();
+            int start = matcher.start();
+            int end = (start+13);
+
+            // If the '&x' is at the end of the string ignore it
+            if (end > text.length()) continue;
+            for (int i = start; i < end; i++) builder.append(chars[i]);
+
+            String hex = builder.toString();
+            hex = hex.replace("&x", "").replace("&", "");
+            text = text.replace(builder.toString(), "&#"+hex);
+        }
+
+        return text;
     }
 }
