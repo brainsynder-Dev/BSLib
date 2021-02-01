c2249db03663f9b9fd04e2c2cbd013cbb8fd70ab
Brian <briansnyder498@gmail.com>
2021-02-01 13:29:11 -0500

    Added fallback for the get methods in JsonFile.java

--- API/src/main/java/lib/brainsynder/files/JsonFile.java
+++ API/src/main/java/lib/brainsynder/files/JsonFile.java
@@ -9,7 +9,7 @@ import com.google.common.base.Charsets;
 import java.io.*;
 import java.nio.charset.Charset;
 
-public class JsonFile implements Movable {
+public abstract class JsonFile implements Movable {
     private final Charset ENCODE = Charsets.UTF_8;
     private JsonObject json;
     protected JsonObject defaults = new JsonObject();
@@ -25,7 +25,7 @@ public class JsonFile implements Movable {
         if (loadDefaults) reload();
     }
 
-    public void loadDefaults() {}
+    public abstract void loadDefaults();
 
     public void reload() {
         try {
@@ -82,54 +82,79 @@ public class JsonFile implements Movable {
         if ((value == null) && hasDefaultKey(key)) value = defaults.get(key);
         return value;
     }
+
     public String getString (String key) {
+        return getString(key, "");
+    }
+    public String getString (String key, String fallback) {
         JsonValue value = getValue(key);
-        if (value == null) return "";
+        if (value == null) return fallback;
         return value.asString();
     }
     public double getDouble (String key) {
+        return getDouble(key, 0);
+    }
+    public double getDouble (String key, double fallback) {
         JsonValue value = getValue(key);
-        if (value == null) return 0;
+        if (value == null) return fallback;
         if (value.isString()) return Double.parseDouble(value.asString());
         return value.asDouble();
     }
     public byte getByte (String key) {
+        return getByte(key, (byte) 0);
+    }
+    public byte getByte (String key, byte fallback) {
         JsonValue value = getValue(key);
-        if (value == null) return 0;
+        if (value == null) return fallback;
         if (value.isString()) return Byte.parseByte(value.asString());
         return (byte) value.asInt();
     }
     public boolean getBoolean (String key) {
+        return getBoolean(key, false);
+    }
+    public boolean getBoolean (String key, boolean fallback) {
         JsonValue value = getValue(key);
-        if (value == null) return false;
+        if (value == null) return fallback;
         try {
             if (value.isString()) return Boolean.parseBoolean(value.asString());
         } catch (IllegalArgumentException | NullPointerException ex) {
-            return false;
+            return fallback;
         }
         return value.asBoolean();
     }
     public short getShort (String key) {
+        return getShort(key, (short) 0);
+    }
+    public short getShort (String key, short fallback) {
         JsonValue value = getValue(key);
-        if (value == null) return 0;
+        if (value == null) return fallback;
         if (value.isString()) return Short.parseShort(value.asString());
         return (short) value.asLong();
     }
     public float getFloat (String key) {
+        return getFloat(key, 0);
+    }
+    public float getFloat (String key, float fallback) {
         JsonValue value = getValue(key);
-        if (value == null) return 0;
+        if (value == null) return fallback;
         if (value.isString()) return Float.parseFloat(value.asString());
         return value.asFloat();
     }
     public long getLong (String key) {
+        return getLong(key, 0);
+    }
+    public long getLong (String key, long fallback) {
         JsonValue value = getValue(key);
-        if (value == null) return 0;
+        if (value == null) return fallback;
         if (value.isString()) return Long.parseLong(value.asString());
         return value.asLong();
     }
     public int getInteger (String key) {
+        return getInteger(key, 0);
+    }
+    public int getInteger (String key, int fallback) {
         JsonValue value = getValue(key);
-        if (value == null) return 0;
+        if (value == null) return fallback;
         if (value.isString()) return Integer.parseInt(value.asString());
         return value.asInt();
     }
