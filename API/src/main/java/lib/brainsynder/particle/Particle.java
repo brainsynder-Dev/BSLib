12d78f73ec1ed01eafe4ea412cbc53a052add7f5
brainsynder <briansnyder498@gmail.com>
2021-07-18 23:52:08 -0400

    1.17 particles

--- API/src/main/java/lib/brainsynder/particle/Particle.java
+++ API/src/main/java/lib/brainsynder/particle/Particle.java
@@ -5,6 +5,7 @@ import lib.brainsynder.ServerVersion;
 
 import java.lang.annotation.Annotation;
 
+// https://minecraft.fandom.com/wiki/Particles
 public enum Particle {
     UNKNOWN,
     // ---- START ---- //
@@ -104,7 +105,25 @@ public enum Particle {
     @EnumVersion(version = ServerVersion.v1_16_R1) REVERSE_PORTAL,
     @EnumVersion(version = ServerVersion.v1_16_R1) SOUL,
     @EnumVersion(version = ServerVersion.v1_16_R1) SOUL_FIRE_FLAME,
-    @EnumVersion(version = ServerVersion.v1_16_R1) WARPED_SPORE
+    @EnumVersion(version = ServerVersion.v1_16_R1) WARPED_SPORE,
+
+    @EnumVersion(version = ServerVersion.v1_17_R1) LIGHT,
+    @EnumVersion(version = ServerVersion.v1_17_R1) DUST_COLOR_TRANSITION(true),
+    @EnumVersion(version = ServerVersion.v1_17_R1) VIBRATION,
+    @EnumVersion(version = ServerVersion.v1_17_R1) FALLING_SPORE_BLOSSOM,
+    @EnumVersion(version = ServerVersion.v1_17_R1) SPORE_BLOSSOM_AIR,
+    @EnumVersion(version = ServerVersion.v1_17_R1) SMALL_FLAME,
+    @EnumVersion(version = ServerVersion.v1_17_R1) SNOWFLAKE,
+    @EnumVersion(version = ServerVersion.v1_17_R1) DRIPPING_DRIPSTONE_LAVA,
+    @EnumVersion(version = ServerVersion.v1_17_R1) FALLING_DRIPSTONE_LAVA,
+    @EnumVersion(version = ServerVersion.v1_17_R1) DRIPPING_DRIPSTONE_WATER,
+    @EnumVersion(version = ServerVersion.v1_17_R1) FALLING_DRIPSTONE_WATER,
+    @EnumVersion(version = ServerVersion.v1_17_R1) GLOW_SQUID_INK,
+    @EnumVersion(version = ServerVersion.v1_17_R1) GLOW,
+    @EnumVersion(version = ServerVersion.v1_17_R1) WAX_ON,
+    @EnumVersion(version = ServerVersion.v1_17_R1) WAX_OFF,
+    @EnumVersion(version = ServerVersion.v1_17_R1) ELECTRIC_SPARK,
+    @EnumVersion(version = ServerVersion.v1_17_R1) SCRAPE
     ; // ---- END ---- //
 
     private final String name;
