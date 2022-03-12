b89cfcf3cb2d10a3a09c4fb1eb91f8dc1769a2e9
brainsynder <briansnyder498@gmail.com>
2022-03-12 14:08:36 -0500

    Cleaned up the ActionMessage.java and TitleMessage.java class files

--- API/src/main/java/lib/brainsynder/nms/ActionMessage.java
+++ API/src/main/java/lib/brainsynder/nms/ActionMessage.java
@@ -1,44 +1,14 @@
 package lib.brainsynder.nms;
 
-import lib.brainsynder.ServerVersion;
-import lib.brainsynder.reflection.Reflection;
 import lib.brainsynder.utils.Colorize;
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.chat.ComponentSerializer;
 import org.bukkit.entity.Player;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Method;
 import java.util.Collection;
-import java.util.UUID;
 
 public class ActionMessage {
-    private Constructor packet;
-    private Object value;
-    private final Method serializerMethod;
     private static ActionMessage actionMessage = null;
-    private Object uuid;
-
-    public ActionMessage() {
-        Class packetPlayOutChatClass = Reflection.getNmsClass("PacketPlayOutChat", "network.protocol.game");
-        try {
-            if (ServerVersion.isEqualOld(ServerVersion.v1_11_R1)) {
-                packet = packetPlayOutChatClass.getConstructor(Reflection.getNmsClass("IChatBaseComponent", "network.chat"), byte.class);
-                value = (byte)2;
-            }else{
-                if (ServerVersion.isEqualNew(ServerVersion.v1_16_R1)) {
-                    uuid = Reflection.getFieldValue(Reflection.getField(Reflection.getNmsClass("SystemUtils", ""), "b"), null);
-                    packet = packetPlayOutChatClass.getConstructor(Reflection.getNmsClass("IChatBaseComponent", "network.chat"), Reflection.getNmsClass("ChatMessageType", "network.chat"), UUID.class);
-                }else{
-                    packet = packetPlayOutChatClass.getConstructor(Reflection.getNmsClass("IChatBaseComponent", "network.chat"), Reflection.getNmsClass("ChatMessageType", "network.chat"));
-                }
-                value = Reflection.invoke(Reflection.getMethod(Reflection.getNmsClass("ChatMessageType", "network.chat"), "a", byte.class), null, (byte)2);
-            }
-        } catch (NoSuchMethodException e) {
-            e.printStackTrace();
-        }
-
-        Class chatSerializer = Reflection.getNmsClass("IChatBaseComponent$ChatSerializer", "network.chat");
-        serializerMethod = Reflection.getMethod(chatSerializer, "a", String.class);
-    }
 
     public void sendMessage(Collection<? extends Player> players, String message) {
         for (Player player : players)
@@ -46,18 +16,8 @@ public class ActionMessage {
     }
 
     public void sendMessage(Player player, String message) {
-        if (ServerVersion.isEqualNew(ServerVersion.v1_16_R1)) {
-            Reflection.sendPacket(player, Reflection.initiateClass(packet, buildMessage(Colorize.translateBungeeHex(message)), value, uuid));
-        }else{
-            Reflection.sendPacket(player, Reflection.initiateClass(packet, buildMessage(Colorize.translateBungeeHex(message))));
-        }
+        player.spigot().sendMessage(ChatMessageType.ACTION_BAR, ComponentSerializer.parse(Colorize.translateBungeeHex(message)));
     }
-
-    private Object buildMessage (String text) {
-        return Reflection.invoke(serializerMethod, null, "{\"text\": \"" + text + "\"}");
-    }
-
-
 
     public static ActionMessage getInstance() {
         if (actionMessage != null) return actionMessage;
--- API/src/main/java/lib/brainsynder/nms/TitleMessage.java
+++ API/src/main/java/lib/brainsynder/nms/TitleMessage.java
@@ -1,55 +1,34 @@
 package lib.brainsynder.nms;
 
-import lib.brainsynder.apache.EnumUtils;
-import lib.brainsynder.reflection.Reflection;
 import lib.brainsynder.utils.Colorize;
 import org.bukkit.entity.Player;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Method;
 import java.util.Collection;
 
-@Deprecated
-/**
- * This is @Deprecated because of Player#sendTitle method (and that fact that subtitles cant be sent with the code...)
- */
 public class TitleMessage {
-    private int fadeIn = 5, stay = 5, fadeOut = 5;
-    private String header=null, subHeader=null;
-
-    private final Constructor packet;
-    private final Object TITLE, SUBTITLE;
-    private final Method serializer;
-
-    public TitleMessage () {
-        Class chatSerializer = Reflection.getNmsClass("IChatBaseComponent$ChatSerializer", "network.chat");
-        serializer = Reflection.getMethod(chatSerializer, "a", String.class);
-
-        // TODO - will not work
-        Class titleAction = Reflection.getNmsClass("PacketPlayOutTitle$EnumTitleAction", "");
-
-        TITLE = EnumUtils.getEnum(titleAction, "TITLE");
-        SUBTITLE = EnumUtils.getEnum(titleAction, "SUBTITLE");
-
-        packet = Reflection.getConstructor(Reflection.getNmsClass("PacketPlayOutTitle"), titleAction, Reflection.getNmsClass("IChatBaseComponent"), Integer.TYPE, Integer.TYPE, Integer.TYPE);
-    }
+    private int fadeIn = 10, stay = 70, fadeOut = 20;
+    private String header = null, subHeader = null;
 
     public TitleMessage setFadeIn(int fadeIn) {
         this.fadeIn = fadeIn;
         return this;
     }
+
     public TitleMessage setFadeOut(int fadeOut) {
         this.fadeOut = fadeOut;
         return this;
     }
+
     public TitleMessage setStay(int stay) {
         this.stay = stay;
         return this;
     }
+
     public TitleMessage setHeader(String header) {
         this.header = header;
         return this;
     }
+
     public TitleMessage setSubHeader(String subHeader) {
         this.subHeader = subHeader;
         return this;
@@ -59,20 +38,9 @@ public class TitleMessage {
         player.sendTitle((header != null) ? Colorize.translateBungeeHex(header) : "",
                 (subHeader != null) ? Colorize.translateBungeeHex(subHeader) : "",
                 fadeIn, stay, fadeOut);
-//        //PacketPlayOutTitle(PacketPlayOutTitle.EnumTitleAction, IChatBaseComponent, fadeIn, stay, fadeOut)
-//        try {
-//            if ((header != null) && (!header.isEmpty())) Reflection.sendPacket(player, Reflection.initiateClass(packet, TITLE, buildMessage(header), fadeIn, stay, fadeOut));
-//            if ((subHeader != null) && (!subHeader.isEmpty())) Reflection.sendPacket(player, Reflection.initiateClass(packet, SUBTITLE, buildMessage(subHeader), fadeIn, stay, fadeOut));
-//        }catch (Exception e) {
-//            player.sendMessage (header + " " + subHeader);
-//        }
     }
 
     public void sendMessage(Collection<? extends Player> players) {
         for (Player player : players) sendMessage(player);
-    }
-
-    private Object buildMessage (String text) {
-        return Reflection.invoke(serializer, null, "{\"text\": \"" + text + "\"}");
     }
 }
