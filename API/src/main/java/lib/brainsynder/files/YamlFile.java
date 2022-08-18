797b83e0677d2b11bc1729765941c6198448fc6b
brainsynder <briansnyder498@gmail.com>
2022-08-18 01:26:58 -0400

    Woops

--- API/src/main/java/lib/brainsynder/files/YamlFile.java
+++ API/src/main/java/lib/brainsynder/files/YamlFile.java
@@ -126,25 +126,16 @@ public abstract class YamlFile implements ConfigurationSection, Movable {
         tempConfig.set(fetchKey(key), configuration.get(key));
     }
 
-    @Override
+
     public List<String> getComments(String s) {
         return null;
     }
-
-    @Override
     public List<String> getInlineComments(String s) {
         return null;
     }
+    public void setComments(String s, List<String> list) {}
+    public void setInlineComments(String s, List<String> list) {}
 
-    @Override
-    public void setComments(String s, List<String> list) {
-
-    }
-
-    @Override
-    public void setInlineComments(String s, List<String> list) {
-
-    }
 
     public void addDefault(String key, Object value, String comment) {
         configuration.addDefault(fetchKey(key), value);
