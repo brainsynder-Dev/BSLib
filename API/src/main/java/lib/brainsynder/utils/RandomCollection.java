2c22b51232b23508957ed8ae2167c22e8fe83da3
brainsynder <briansnyder498@gmail.com>
2023-03-13 22:38:05 -0400

    Added the RandomCollection class

---
+++ API/src/main/java/lib/brainsynder/utils/RandomCollection.java
@@ -0,0 +1,78 @@
+package lib.brainsynder.utils;
+
+import java.util.*;
+
+public class RandomCollection<E> {
+    private final NavigableMap<Double, E> map;
+    private final Random random;
+    private double total;
+
+    public static <E> E randomize(Collection<E> list) {
+        return randomize(list, 50);
+    }
+
+    public static <E> E randomize(Collection<E> list, double percent) {
+        RandomCollection<E> collection = new RandomCollection<>();
+        list.forEach(e -> collection.add(percent, e));
+        return collection.next();
+    }
+
+    public static <E> RandomCollection<E> fromCollection(Collection<E> list) {
+        return fromCollection(list, 50);
+    }
+
+    public static <E> RandomCollection<E> fromCollection(Collection<E> list, double percent) {
+        RandomCollection<E> collection = new RandomCollection<>();
+        list.forEach(e -> collection.add(percent, e));
+        return collection;
+    }
+
+    public RandomCollection() {
+        this(new Random());
+    }
+
+    public RandomCollection(Random var1) {
+        this.map = new TreeMap<>();
+        this.total = 0.0D;
+        this.random = var1;
+    }
+
+    public void add(E value) {
+        add(50, value);
+    }
+
+    public void add(double percent, E value) {
+        if (percent > 0.0D) {
+            this.total += percent;
+            this.map.put(this.total, value);
+        }
+    }
+
+    public Collection<E> values() {
+        return map.values();
+    }
+
+    public E next() {
+        double var1 = this.random.nextDouble() * this.total;
+        return this.map.ceilingEntry(var1).getValue();
+    }
+
+    public boolean isEmpty() {
+        return map.isEmpty();
+    }
+
+    public int getSize() {
+        return map.size();
+    }
+
+    // Will fetch the next random value, while also removing it from the selections
+    public E nextRemove() {
+        if (map.isEmpty()) return null;
+        double var1 = this.random.nextDouble() * this.total;
+        Map.Entry<Double, E> entry = this.map.ceilingEntry(var1);
+        E value = entry.getValue();
+        this.total -= entry.getKey();
+        map.remove(entry.getKey());
+        return value;
+    }
+}
