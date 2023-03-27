2cd6c3f2fd3402f04009959843a1f5fa7bc84bf2
brainsynder <briansnyder498@gmail.com>
2023-03-27 02:22:59 -0400

    Added more methods to the BukkitUtils class

--- API/src/main/java/lib/brainsynder/utils/BukkitUtils.java
+++ API/src/main/java/lib/brainsynder/utils/BukkitUtils.java
@@ -3,16 +3,312 @@ package lib.brainsynder.utils;
 import lib.brainsynder.math.MathUtils;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.World;
 import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
 import org.bukkit.util.Vector;
 
-import java.util.Iterator;
-import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Predicate;
 
 public class BukkitUtils {
+
+    /**
+     * If the entity is null, dead, or offline, return false. Otherwise, return true.
+     *
+     * @param entity The entity to check
+     * @return A boolean value.
+     */
+    public static boolean isValid(Entity entity) {
+        if (entity == null) return false;
+        if (entity.isValid() || (!entity.isDead())) return true;
+        if (entity instanceof Player) return ((Player) entity).isOnline();
+        return false;
+    }
+
+    /**
+     * Converts a time in a given unit to ticks.
+     *
+     * @param time The time you want to convert
+     * @param unit The unit of time to convert from.
+     * @return The number of ticks in the given time.
+     */
+    public static long convertTime2Ticks(long time, TimeUnit unit) {
+        return (unit.toSeconds(time) * 20);
+    }
+
+    /**
+     * For each block in the cube, if it's on the edge of the cube, add it to the list.
+     *
+     * @param loc The center of the cube
+     * @param distance The distance between each block.
+     * @return A list of locations that are on the outside of a cube.
+     */
+    public List<Location> getHollowCube(Location loc, double distance) {
+        List<Location> result = new ArrayList<>();
+        World world = loc.getWorld();
+        double minX = loc.getBlockX();
+        double minY = loc.getBlockY();
+        double minZ = loc.getBlockZ();
+        double maxX = loc.getBlockX() + 1;
+        double maxY = loc.getBlockY() + 1;
+        double maxZ = loc.getBlockZ() + 1;
+
+        for (double x = minX; x <= maxX; x = Math.round((x + distance) * 1e2) / 1e2) {
+            for (double y = minY; y <= maxY; y = Math.round((y + distance) * 1e2) / 1e2) {
+                for (double z = minZ; z <= maxZ; z = Math.round((z + distance) * 1e2) / 1e2) {
+                    int components = 0;
+                    if (x == minX || x == maxX) components++;
+                    if (y == minY || y == maxY) components++;
+                    if (z == minZ || z == maxZ) components++;
+                    if (components >= 2) {
+                        result.add(new Location(world, x, y, z));
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+    /**
+     * "Get all the blocks in a radius around a location, optionally hollow, optionally ignore air, optionally filter by
+     * block type."
+     *
+     * The first parameter is the location to get the blocks around. The second parameter is the radius. The third
+     * parameter is whether or not to ignore the blocks in the center. The fourth parameter is whether or not to ignore air
+     * blocks. The fifth parameter is a predicate that determines whether or not to include a block
+     *
+     * @param location The location to get the blocks around.
+     * @param radius The radius of the sphere.
+     * @param hollow If true, the blocks on the edge of the radius will not be included.
+     * @return A list of blocks in a radius.
+     */
+    public static List<Block> getBlocksInRadius(Location location, int radius, boolean hollow) {
+        return getBlocksInRadius(location, radius, hollow, false, block -> true);
+    }
+
+    /**
+     * "Returns a list of blocks in a radius around a location, optionally ignoring height, optionally hollow, and
+     * optionally filtering the blocks."
+     *
+     * The first parameter is the location to get the blocks around. The second parameter is the radius. The third
+     * parameter is whether or not to ignore height. The fourth parameter is whether or not to make the radius hollow. The
+     * fifth parameter is a predicate that filters the blocks
+     *
+     * @param location The location to get the blocks around.
+     * @param radius The radius of the sphere.
+     * @param hollow If true, the blocks on the outer edge of the radius will not be included.
+     * @param ignoreHeight If true, the height of the location will be ignored.
+     * @return A list of blocks in a radius.
+     */
+    public static List<Block> getBlocksInRadius(Location location, int radius, boolean hollow, boolean ignoreHeight) {
+        return getBlocksInRadius(location, radius, hollow, ignoreHeight, block -> true);
+    }
+
+    /**
+     * "Get all blocks in a radius around a location, optionally ignoring height, optionally hollow, and optionally only
+     * including blocks that match a predicate."
+     *
+     * The first parameter is the location to get the blocks around. The second parameter is the radius to get the blocks
+     * in. The third parameter is whether or not to ignore height. The fourth parameter is whether or not to make the
+     * radius hollow. The fifth parameter is a predicate that the block must match in order to be included in the list
+     *
+     * @param location The center of the sphere
+     * @param radius The radius of the sphere.
+     * @param hollow If true, the blocks in the middle of the sphere will be ignored.
+     * @param ignoreHeight If true, the y-axis will be ignored.
+     * @param blockPredicate A predicate that returns true if the block should be added to the list.
+     * @return A list of blocks
+     */
+    public static List<Block> getBlocksInRadius(Location location, int radius, boolean hollow, boolean ignoreHeight, Predicate<Block> blockPredicate) {
+        List<Block> blocks = new ArrayList<>();
+
+        int bX = location.getBlockX();
+        int bY = location.getBlockY();
+        int bZ = location.getBlockZ();
+
+        if (ignoreHeight) {
+            for (int x = bX - radius; x <= bX + radius; x++) {
+                for (int z = bZ - radius; z <= bZ + radius; z++) {
+                    double distance = (bX - x) * (bX - x) + (bZ - z) * (bZ - z);
+                    if ((distance < radius * radius) && ((!hollow) || (distance >= (radius - 1) * (radius - 1)))) {
+                        Location loc = new Location(location.getWorld(), x, bY, z);
+
+                        if (blockPredicate.test(loc.getBlock())) blocks.add(loc.getBlock());
+                    }
+                }
+            }
+            return blocks;
+        }
+
+        for (int x = bX - radius; x <= bX + radius; x++) {
+            for (int y = bY - radius; y <= bY + radius; y++) {
+                for (int z = bZ - radius; z <= bZ + radius; z++) {
+                    double distance = (bX - x) * (bX - x) + (bY - y) * (bY - y) + (bZ - z) * (bZ - z);
+                    if ((distance < radius * radius) && ((!hollow) || (distance >= (radius - 1) * (radius - 1)))) {
+                        Location loc = new Location(location.getWorld(), x, y, z);
+
+                        if (blockPredicate.test(loc.getBlock())) blocks.add(loc.getBlock());
+                    }
+                }
+            }
+        }
+        return blocks;
+    }
+
+    /**
+     * It takes a location and a location to look at, and returns a new location with the same X, Y, and Z, but with the
+     * Yaw and Pitch set to look at the second location
+     *
+     * @param loc The location you want to look at.
+     * @param lookat The location you want to look at.
+     * @return A Location object.
+     */
+    public static Location lookAt(Location loc, Location lookat) {
+        loc = loc.clone();
+        double dx = lookat.getX() - loc.getX();
+        double dz = lookat.getZ() - loc.getZ();
+        if (dx != 0) {
+            if (dx < 0) {
+                loc.setYaw((float) (1.5 * Math.PI));
+            } else {
+                loc.setYaw((float) (0.5 * Math.PI));
+            }
+            loc.setYaw(loc.getYaw() - (float) Math.atan(dz / dx));
+        } else if (dz < 0) {
+            loc.setYaw((float) Math.PI);
+        }
+        loc.setYaw(-loc.getYaw() * 180f / (float) Math.PI);
+        return loc;
+    }
+
+    public static BlockFace getCardinalDirection(final double n) {
+        double n2 = (n - 90.0) % 360.0;
+        if (n2 < 0.0) {
+            n2 += 360.0;
+        }
+        if (0.0 <= n2 && n2 < 22.5) {
+            return BlockFace.WEST;
+        }
+        if (22.5 <= n2 && n2 < 67.5) {
+            return BlockFace.NORTH_WEST;
+        }
+        if (67.5 <= n2 && n2 < 112.5) {
+            return BlockFace.NORTH;
+        }
+        if (112.5 <= n2 && n2 < 157.5) {
+            return BlockFace.NORTH_EAST;
+        }
+        if (157.5 <= n2 && n2 < 202.5) {
+            return BlockFace.EAST;
+        }
+        if (202.5 <= n2 && n2 < 247.5) {
+            return BlockFace.SOUTH_EAST;
+        }
+        if (247.5 <= n2 && n2 < 292.5) {
+            return BlockFace.SOUTH;
+        }
+        if (292.5 <= n2 && n2 < 337.5) {
+            return BlockFace.SOUTH_WEST;
+        }
+        if (337.5 <= n2 && n2 < 360.0) {
+            return BlockFace.NORTH;
+        }
+        return null;
+    }
+
+    public static BlockFace getBlockCardinal(final double n) {
+        double n2 = (n - 90.0) % 360.0;
+        if (n2 < 0.0) {
+            n2 += 360.0;
+        }
+        if (0.0 <= n2 && n2 < 22.5) {
+            return BlockFace.WEST;
+        }
+        if (22.5 <= n2 && n2 < 67.5) {
+            return BlockFace.WEST;
+        }
+        if (67.5 <= n2 && n2 < 112.5) {
+            return BlockFace.NORTH;
+        }
+        if (112.5 <= n2 && n2 < 157.5) {
+            return BlockFace.NORTH;
+        }
+        if (157.5 <= n2 && n2 < 202.5) {
+            return BlockFace.EAST;
+        }
+        if (202.5 <= n2 && n2 < 247.5) {
+            return BlockFace.EAST;
+        }
+        if (247.5 <= n2 && n2 < 292.5) {
+            return BlockFace.SOUTH;
+        }
+        if (292.5 <= n2 && n2 < 337.5) {
+            return BlockFace.SOUTH;
+        }
+        if (337.5 <= n2 && n2 < 360.0) {
+            return BlockFace.NORTH;
+        }
+        return null;
+    }
+
+    public static BlockFace getNextDirection(final BlockFace blockFace) {
+        if (blockFace == BlockFace.NORTH) {
+            return BlockFace.EAST;
+        }
+        if (blockFace == BlockFace.EAST) {
+            return BlockFace.SOUTH;
+        }
+        if (blockFace == BlockFace.SOUTH) {
+            return BlockFace.WEST;
+        }
+        if (blockFace == BlockFace.WEST) {
+            return BlockFace.NORTH;
+        }
+        if (blockFace == BlockFace.NORTH_EAST) {
+            return BlockFace.EAST;
+        }
+        if (blockFace == BlockFace.SOUTH_EAST) {
+            return BlockFace.SOUTH;
+        }
+        if (blockFace == BlockFace.SOUTH_WEST) {
+            return BlockFace.WEST;
+        }
+        if (blockFace == BlockFace.NORTH_WEST) {
+            return BlockFace.NORTH;
+        }
+        return BlockFace.NORTH;
+    }
+
+    public static Vector spread(Vector vector, float spread) {
+        Random random = new Random();
+        double x = random.nextDouble() * spread;
+        double y = random.nextDouble() * spread;
+        double z = random.nextDouble() * spread;
+
+        if (random.nextBoolean()) {
+            x = vector.getX() + x;
+        } else {
+            x = vector.getX() - x;
+        }
+        if (random.nextBoolean()) {
+            y = vector.getY() + y;
+        } else {
+            y = vector.getY() - y;
+        }
+        if (random.nextBoolean()) {
+            z = vector.getZ() + z;
+        } else {
+            z = vector.getZ() - z;
+        }
+
+        return new Vector(x, y, z);
+    }
+
+
     public static TreeSet<String> sortKey(Set<String> toSort) {
         TreeSet sortedSet = new TreeSet();
         Iterator i$ = toSort.iterator();
